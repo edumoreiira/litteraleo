e44879fbb510949b6d7946b2e5e050febddb4b12
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-06-02 09:35:45 -0300

    feat: add searchbar layout

---
+++ src/app/components/shared/searchbar/searchbar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+ selector: 'app-searchbar',
+ templateUrl: './searchbar.component.html',
+ imports: [],
+})
+export class SearchbarComponent {
+  // This component can be extended with search functionality in the future
+  // Currently, it serves as a placeholder for a search bar
+}
