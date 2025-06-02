4d12d3c1b505d5d488bd99d6710d22b7409f4384
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-06-01 23:59:07 -0300

    feat(navbar): add hide on scroll behavior

--- src/app/components/layout/navbar.component.ts
+++ src/app/components/layout/navbar.component.ts
@@ -1,11 +1,32 @@
-import { Component } from "@angular/core";
-import { ButtonComponent } from "../base/Button/button.component";
+import { AfterViewChecked, ChangeDetectionStrategy, Component, ElementRef, HostListener, inject, signal } from '@angular/core';
+import { ButtonComponent } from '../base/Button/button.component';
 
 @Component({
-    selector: "app-navbar",
-    templateUrl: "./navbar.component.html",
-    imports: [ButtonComponent],
+  selector: 'app-navbar',
+  host: {
+    class: 'sticky block top-[0] left-[0] w-full z-50 bg-white px-12 py-4 flex items-center justify-between',
+    '[attr.style]': '"transform: translateY(-" + navbarOffset() + "px);"',
+  },
+  templateUrl: './navbar.component.html',
+  imports: [ButtonComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NavbarComponent {
-    
+export class NavbarComponent implements AfterViewChecked {
+  el = inject(ElementRef);
+  navbarOffset = signal(0);
+  lastScrollTop = 0;
+  maxOffset = 90;
+
+  ngAfterViewChecked(): void {
+    this.maxOffset = this.el.nativeElement.offsetHeight;
+  }
+  @HostListener('window:scroll')
+  onWindowScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const delta = scrollTop - this.lastScrollTop;
+
+    this.navbarOffset.update(offset => offset + delta);
+    this.navbarOffset.update(offset => Math.min(Math.max(offset, 0), this.maxOffset));
+    this.lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  }
 }
