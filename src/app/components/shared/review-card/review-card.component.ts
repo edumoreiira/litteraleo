2e913500b01e93c69f8fbc41cf967c25ee2bf556
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-06-04 19:03:23 -0300

    feat(review-card): add layout

---
+++ src/app/components/shared/review-card/review-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { TextDirective } from '../../../directives/ui/text.directive';
+
+@Component({
+  selector: 'app-review-card',
+  imports: [TextDirective],
+  templateUrl: './review-card.component.html',
+})
+export class ReviewCardComponent {
+
+}
