b71cd4b69e63662fe5ddcfaa57cc665258627379
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-06-05 21:35:05 -0300

    feat(recommendation-card): add responsivity

--- src/app/components/layout/recommendation-card/recommendation-card.component.ts
+++ src/app/components/layout/recommendation-card/recommendation-card.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { TitleDirective } from '../../../directives/ui/title.directive';
 import { TextDirective } from '../../../directives/ui/text.directive';
 import { RateComponent } from '../../shared/rate/rate.component';
 import { ButtonComponent } from '../../base/Button/button.component';
+import { DocumentListenerService } from '../../../services/platform/document-listener.service';
 
 @Component({
   selector: 'app-recommendation-card',
   host: {
-    class: 'block bg-card text-card-fg rounded p-3 flex gap-4'
+    class: 'block bg-card text-card-fg rounded p-3 flex gap-4 md:flex-row flex-col'
   },
   imports: [TextDirective, RateComponent, ButtonComponent],
   templateUrl: './recommendation-card.component.html',
 })
 export class RecommendationCardComponent {
+  protected documentListener = inject(DocumentListenerService);
   tags = [
     "Investigação", "Ação", "Aventura", "Ficção Científica",
     "Drama", "Comédia"
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,10 +4,11 @@ import { TitleDirective } from '../../directives/ui/title.directive';
 import { TextDirective } from '../../directives/ui/text.directive';
 import { CardSlider } from "../../components/shared/card-slider/card-slider.component";
 import { ReviewCardComponent } from '../../components/shared/review-card/review-card.component';
+import { RecommendationCardComponent } from "../../components/layout/recommendation-card/recommendation-card.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent, TitleDirective, TextDirective, CardSlider, ReviewCardComponent],
+  imports: [ButtonComponent, TitleDirective, TextDirective, CardSlider, ReviewCardComponent, RecommendationCardComponent],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
