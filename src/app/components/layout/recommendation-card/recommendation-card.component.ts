11c9844836d5a9a7a534a8f9d7989600ae7325ec
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-06-05 23:53:33 -0300

    feat(recommendation-card): ensure image has 1:1.5 aspect ratio

--- src/app/components/layout/recommendation-card/recommendation-card.component.ts
+++ src/app/components/layout/recommendation-card/recommendation-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { AfterViewChecked, Component, ElementRef, inject, input, viewChild } from '@angular/core';
 import { TitleDirective } from '../../../directives/ui/title.directive';
 import { TextDirective } from '../../../directives/ui/text.directive';
 import { RateComponent } from '../../shared/rate/rate.component';
@@ -8,15 +8,25 @@ import { DocumentListenerService } from '../../../services/platform/document-lis
 @Component({
   selector: 'app-recommendation-card',
   host: {
-    class: 'block bg-card text-card-fg rounded p-2 flex gap-4 md:flex-row flex-col'
+    class: 'bg-card text-card-fg rounded p-2 flex gap-4 md:flex-row flex-col'
   },
   imports: [TextDirective, RateComponent, ButtonComponent],
   templateUrl: './recommendation-card.component.html',
 })
-export class RecommendationCardComponent {
+export class RecommendationCardComponent implements AfterViewChecked {
   protected documentListener = inject(DocumentListenerService);
+  imgContainer = viewChild.required('imgContainer', { read: ElementRef<HTMLDivElement> });
+  description = input<string>() // max 1000 characters
   tags = [
     "Investigação", "Ação", "Aventura", "Ficção Científica",
     "Drama", "Comédia"
   ]
+
+  ngAfterViewChecked(): void {
+    const imgContainer = this.imgContainer().nativeElement as HTMLDivElement;
+    const height = imgContainer.offsetHeight;
+    const width = height / 1.5; // 1.5 is the aspect ratio (height / width)
+    imgContainer.style.width = `${width}px`;
+    imgContainer.style.minWidth = `${width}px`;
+  }
 }
