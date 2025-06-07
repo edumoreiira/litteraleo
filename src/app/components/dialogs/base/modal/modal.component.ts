4decbf1bf2ee440e43d40853c77d8301266e20df
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-06-07 13:27:27 -0300

    chore: implement modal service

---
+++ src/app/components/dialogs/base/modal/modal.component.ts
@@ -0,0 +1,91 @@
+import { AfterViewInit, ChangeDetectionStrategy, Component, ComponentRef, effect, ElementRef, EnvironmentInjector, HostListener, inject, input, output, Type, ViewChild, ViewContainerRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { createAnimation, createQueryAnimations } from '../../../../angular-animations/animations.utils';
+import { DocumentListenerService } from '../../../../services/platform/document-listener.service';
+import { ModalSizeUnit } from '../../../../models/modal.interface';
+
+@Component({
+  selector: 'modal',
+  standalone: true,
+  imports: [CommonModule],
+  host: { '[@queryAnimationsModal]': '', class: 'modal' },
+  template: `
+  <!-- <div class="modal"> host div-->
+  <div class="modal__content" @popUp focusTrap
+    [ngStyle]="{'width': 'min(' + minWidth() + ', 100%)'}"
+    [attr.role]="role()" [attr.aria-labelledby]="ariaLabelledBy()" [attr.aria-describedby]="ariaDescribedBy()"
+    [attr.aria-label]="ariaLabel()" aria-modal="true"> 
+    <button #ModalCloseButton class="close-sr-only" aria-label="Close Modal" (keydown)="onKeydownClose($event)"></button>
+        <ng-template #vc></ng-template>
+    </div>
+    <div class="modal__backdrop" @fadeInOut (click)="onCloseModal.emit()"></div>
+  <!-- </div> -->`,
+  styleUrl: './modal.component.scss',
+  animations: [
+    createAnimation('popUp', { duration: '200ms', transform: 'scale(1.1)', opacity: '0' }),
+    createAnimation('fadeInOut', { duration: '250ms', opacity: '0' }),
+    createQueryAnimations('queryAnimationsModal', '@popUp, @fadeInOut')
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+
+
+export class ModalComponent<T> implements AfterViewInit {
+  // injections
+  private envInjector = inject(EnvironmentInjector);
+  private documentListener = inject(DocumentListenerService);
+
+  //content inputs
+  public component = input.required<Type<T>>();
+  public componentInputs = input<Partial<T>>({});
+
+  // modal controls
+  onCloseModal = output();
+  minWidth = input<ModalSizeUnit>('350px');
+  role = input.required<string>();
+  ariaLabelledBy = input<string>();
+  ariaDescribedBy = input<string>();
+  ariaLabel = input<string>();
+
+  @ViewChild('vc', { read: ViewContainerRef, static: true }) vc!: ViewContainerRef;
+  @ViewChild('ModalCloseButton') modalCloseButton!: ElementRef;
+
+  public cmpRef?: ComponentRef<T>;
+
+  constructor(){
+    effect(() => {
+      const event = this.documentListener.event$();
+      if(event instanceof KeyboardEvent) {
+        this.handleKeyDown(event);
+      }
+    })
+  }
+
+
+  ngAfterViewInit(): void {
+    // focus on the close button so the user get the focus on the modal and trapped inside it
+    this.modalCloseButton.nativeElement.focus();
+    
+    // create component()
+    this.cmpRef = this.vc.createComponent(this.component(), { environmentInjector: this.envInjector });
+
+    // set inputs
+    for (const [key, value] of Object.entries(this.componentInputs())) {
+      this.cmpRef.setInput(key, value);
+    }
+  }
+
+  onKeydownClose(event: KeyboardEvent) {
+     if(event.key === 'Enter' || event.key === ' ') {
+       this.onCloseModal.emit();
+     }
+   } 
+
+  handleKeyDown(event: KeyboardEvent) {
+    if(event.key === 'Escape') {
+      this.onCloseModal.emit();
+    }
+  }
+    
+  
+}
---
+++ src/app/services/ui/modal.service.ts
@@ -0,0 +1,70 @@
+import { ApplicationRef, ComponentRef, createComponent, EnvironmentInjector, inject, Injectable, Injector, output, Type } from "@angular/core";
+import { DOCUMENT } from "@angular/common";
+import { Subject } from "rxjs";
+import { ModalComponent } from "../../components/dialogs/base/modal/modal.component";
+import { ModalConfig, ModalRef } from "../../models/modal.interface";
+
+@Injectable( { providedIn: 'root' })
+export class ModalService {
+    private appRef = inject(ApplicationRef);
+    private injector = inject(Injector);
+    private envInjector = inject(EnvironmentInjector);
+    private document = inject(DOCUMENT);
+    
+    public open<T>(
+        component: Type<T>,
+        config: ModalConfig<T>,
+    ): ModalRef<T> {
+        const closed$ = new Subject<void>;
+        
+        //create ModalComponent
+        const modalCompRef = createComponent<ModalComponent<T>>(ModalComponent, {
+            environmentInjector: this.envInjector,
+            elementInjector: this.injector
+        });
+
+        // set required signals from ModalComponent
+        this.setInputs(config, modalCompRef, component);
+        
+        // connect angular view tree
+        this.appRef.attachView(modalCompRef.hostView);
+        
+        
+        // renders on DOM
+        const domElem = (modalCompRef.hostView as any).rootNodes[0] as HTMLElement;
+        this.document.body.appendChild(domElem);
+        
+        modalCompRef.changeDetectorRef.detectChanges();
+
+        const modalRef: ModalRef<T> = {
+          componentRef: modalCompRef.instance.cmpRef!,
+          close: () => {
+            this.close(modalCompRef);
+            closed$.next();
+            closed$.complete();
+          },
+          afterClosed$: closed$.asObservable()
+        };
+        
+        modalCompRef.instance.onCloseModal.subscribe(() => modalRef.close());
+        this.document.body.classList.add('overflow-hidden'); // prevent body scroll when modal is open
+        return modalRef
+    }
+    
+
+    private close<T>(modalRef: ComponentRef<ModalComponent<T>>) {
+        this.document.body.classList.remove('overflow-hidden'); // re-enable body scroll
+        this.appRef.detachView(modalRef.hostView);
+        modalRef.destroy();
+    }
+
+    private setInputs<T>(config: ModalConfig<T>, modalRef: ComponentRef<ModalComponent<T>>, component: Type<T>) {
+        modalRef.setInput('component', component);
+        modalRef.setInput('componentInputs', config.componentInputs ?? {});
+        modalRef.setInput('role', config.role);
+        if (config.minWidth)       modalRef.setInput('minWidth', config.minWidth);
+        if (config.ariaLabelledBy) modalRef.setInput('ariaLabelledBy', config.ariaLabelledBy);
+        if (config.ariaDescribedBy)modalRef.setInput('ariaDescribedBy', config.ariaDescribedBy);
+        if (config.ariaLabel)      modalRef.setInput('ariaLabel', config.ariaLabel);
+    }
+}
