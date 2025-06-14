ec9935c2f6cc4ce8cb89a5a71e9c0fca32891869
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-06-14 14:30:34 -0300

    feat(input): improve handling of input state reacting to host element changes

--- src/app/components/base/input/input.component.ts
+++ src/app/components/base/input/input.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, forwardRef, HostListener, inject, input, signal, viewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, forwardRef, HostListener, inject, input, OnDestroy, OnInit, signal, viewChild } from '@angular/core';
 import { ControlValueAccessor, NG_VALUE_ACCESSOR, NgControl } from '@angular/forms';
 
 type InputTypes = 'text' | 'email' | 'password' | 'number' | 'tel' | 'url';
@@ -19,7 +19,7 @@ type InputTypes = 'text' | 'email' | 'password' | 'number' | 'tel' | 'url';
     class="w-full px-3 py-1.5 border border-input rounded-lg outline-none placeholder:text-muted-fg text-fg text-sm shadow-xs
     focus-visible:ring-[3px] focus-visible:border-ring focus-visible:ring-ring/20 transition-all
     disabled:cursor-not-allowed disabled:opacity-50
-    aria-invalid:border-destructive aria-invalid:ring-destructive/20 aria-invalid:ring-[3px]"
+    aria-invalid:border-destructive focus-visible:aria-invalid:ring-destructive/20"
     #input 
     [id]="identifier()"
     [type]="type()"
@@ -38,20 +38,32 @@ type InputTypes = 'text' | 'email' | 'password' | 'number' | 'tel' | 'url';
   ],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InputComponent implements ControlValueAccessor{
+export class InputComponent implements ControlValueAccessor, OnInit, OnDestroy{
   el = inject(ElementRef);
   // inputs
   identifier = input.required<string>();
   label = input.required<string>();
   placeholder = input<string>('');
   type = input<InputTypes>('text');
-  setInvalid = input<boolean>(false);
   // signals
   value = signal('');
   invalid = signal(false);
   // viewchild
   inputRef = viewChild.required('input', { read: ElementRef });
+  // 
+  private mutationObserver!: MutationObserver
 
+
+  ngOnInit(): void {
+    // Initialize the input value from the control's value
+    this.validateInputState();
+  }
+
+  ngOnDestroy(): void {
+    if (this.mutationObserver) {
+      this.mutationObserver.disconnect();
+    }
+  }
 
 
   onInput(event: Event): void {
@@ -62,7 +74,7 @@ export class InputComponent implements ControlValueAccessor{
   }
   onBlur() {
     this.onTouched();
-    this.checkValidity();
+    this.validateInputState();
   }
   // ControlValueAccessor methods
   onChange: any = () => {};
@@ -82,12 +94,29 @@ export class InputComponent implements ControlValueAccessor{
     }
   }
 
-  private checkValidity() {
-    const hostEl = this.el.nativeElement;
-    if(this.value() === '') {
-      this.invalid.set(false);
-      return;
+validateInputState() {
+  const hostEl = this.el.nativeElement;
+  let debounceTimeout: ReturnType<typeof setTimeout>
+
+  this.mutationObserver = new MutationObserver(() => {
+    if (debounceTimeout) {
+      clearTimeout(debounceTimeout);
     }
-    this.invalid.set(hostEl.classList.contains('ng-invalid') && hostEl.classList.contains('ng-dirty'));
-  }
+
+    debounceTimeout = setTimeout(() => { // debounce to avoid multiple calls
+      const currentClassList = hostEl.classList;
+      if (currentClassList.contains('ng-invalid') && currentClassList.contains('ng-dirty')) {
+        this.invalid.set(true);
+      } else {
+        this.invalid.set(false);
+      }
+    }, 50);
+  });
+
+  this.mutationObserver.observe(hostEl, {
+    attributes: true,
+    attributeFilter: ['class'],
+  });
+}
+
 }
