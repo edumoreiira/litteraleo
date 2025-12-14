581f6b2c54a8db1ad5b2d6c9cd342645032aa5ef
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-12-13 23:33:17 -0300

    feat(dialog): add confirmation dialog

---
+++ src/app/components/dialogs/base/confirmation-dialog/confirmation-dialog.component.ts
@@ -0,0 +1,36 @@
+import { ChangeDetectionStrategy, Component, inject, input, output } from "@angular/core";
+import { ButtonComponent } from "app/components/base/Button/button.component";
+
+@Component({
+  selector: "app-confirmation-dialog",
+  template: `
+    <h1 class="font-semibold text-2xl mb-2">{{ title() }}</h1>
+    <p class="text-neutral-600">{{ message() }}</p>
+    <div class="flex justify-between gap-2 mt-8">
+      <button 
+      class="px-5"
+      app-button 
+      variant="outline" 
+      (click)="cancel.emit()"
+      >{{ cancelText() }}</button>
+      <button
+      class="px-5"
+      app-button
+      [class.!bg-destructive]="variant() === 'destructive'"
+      [class.!text-destructive-fg]="variant() === 'destructive'"
+      (click)="confirm.emit()"
+      >{{ confirmText() }}</button>
+    </div>
+  `,
+  imports: [ButtonComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ConfirmationDialogComponent {
+  title = input.required<string>();
+  message = input.required<string>();
+  confirmText = input<string>('Confirmar');
+  cancelText = input<string>('Cancelar');
+  variant = input<'destructive' | 'info'>('info');
+  confirm = output<void>();
+  cancel = output<void>();
+}
---
+++ src/app/services/ui/dialog.service.ts
@@ -0,0 +1,40 @@
+import { inject, Injectable } from "@angular/core";
+import { ModalService } from "./modal.service";
+import { ConfirmationDialogComponent } from "app/components/dialogs/base/confirmation-dialog/confirmation-dialog.component";
+
+interface ConfirmationDialogData {
+  title: string;
+  message: string;
+  confirmText?: string;
+  cancelText?: string;
+  variant?: 'destructive' | 'info';
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DialogService {
+  private modal = inject(ModalService)
+  // onConfirm can be updated to use Promises if needed in the future
+  openConfirmationDialog(data: ConfirmationDialogData, actions: { onConfirm?: () => void, onCancel?: () => void }) {
+    const modalRef =this.modal.open(ConfirmationDialogComponent, { 
+      role: 'alertdialog',
+      maxWidth: '500px',
+      componentInputs: {
+        title: data.title,
+        message: data.message,
+        confirmText: data.confirmText,
+        cancelText: data.cancelText,
+        variant: data.variant
+      }
+    })
+    modalRef.componentRef.instance.confirm.subscribe(() => {
+      if (actions.onConfirm) actions.onConfirm();
+      modalRef.close();
+    });
+    modalRef.componentRef.instance.cancel.subscribe(() => {
+      if (actions.onCancel) actions.onCancel();
+      modalRef.close();
+    });
+  }
+}
