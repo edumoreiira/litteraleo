52e95698d7a85c87397a84663867c8f81bb9cbf9
Eduardo Moreira <dudu_moreira9@hotmail.com>
2025-12-26 11:52:26 -0300

    fix: bootstrap express on vercel to fix SSR issues

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'starter-kit' title`, () => {
+  it(`should have the 'litteraleo' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('starter-kit');
+    expect(app.title).toEqual('litteraleo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, starter-kit');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, litteraleo');
   });
 });
